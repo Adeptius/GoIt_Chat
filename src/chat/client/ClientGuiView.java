6396ba2798ae6ad6d2ee1157f889589d261b077f
andreas2000 <pyatnitskiy.a@gmail.com>
2016-08-13 12:46:32 +0300

    Refactoring. Change packages1

--- src/chat/client/ClientGuiView.java
+++ src/chat/client/ClientGuiView.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ClientGuiView {
+    //1
     private final ClientGuiController controller;
 
     private JFrame frame = new JFrame("Чат");
